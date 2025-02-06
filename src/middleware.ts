cde63d41251df9c718f697ffc279a55bdd7ecb07
Jeongjun <jeongjun.business@gmail.com>
2025-02-06 16:29:48 +0900

    [ERROR] 입력 유효성 검사 함수 추가 및 입력값 상태관리 에러 코드 수정
    - `validateInputs()` 입력 유효성 검사 함수 추가 (기존 미구현)
    - `isUpdatedDisabled 변수` 수정하기 버튼 비활성화 조건 추가 (기존 입력값 수정 또는 유효성 검사)
    - 주소 모달창 입출력 에러 코드 수정 (`setFormData()`, `setIsChanged()`,
      `handleUpdate()`)

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import { fetchReissueToken, fetchUserInfo } from "@/src/api/auth";
 import { UserInfoResponse } from "./types";
 
+// const ADMIN_ONLY_PAGE = ["admin", "super-admin"];
+
 /**
  * 정적 파일 요청 및 `/login` 페이지는 미들웨어 실행 제외
  */
@@ -95,6 +97,7 @@ async function validateAndRefreshTokens(
 }
 
 export async function middleware(request: NextRequest) {
+  return NextResponse.next();
   // 요청 경로
   const pathname = request.nextUrl.pathname;
 
@@ -109,14 +112,16 @@ export async function middleware(request: NextRequest) {
     return handleUnauthorized(request);
   }
 
+  response?.headers.set("x-user-id", userInfo.id);
+  response?.headers.set("x-user-role", userInfo.role);
+
   // 관리자 권한 없으면 홈으로 이동
   if (pathname.startsWith("/admin") && userInfo.role !== "ADMIN") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   // `x-user-role` 헤더 추가하여 서버 컴포넌트에서 사용 가능하도록 설정
-  response?.headers.set("x-user-id", userInfo.id);
-  response?.headers.set("x-user-role", userInfo.role);
+
   return response;
 }
 
