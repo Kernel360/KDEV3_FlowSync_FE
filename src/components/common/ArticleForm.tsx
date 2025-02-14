e74bed4079007b72994c1079492cb267e2302804
doctorsean <55120852+doctorsean@users.noreply.github.com>
2025-02-14 14:31:36 +0900

    링크 존재 여부 검사

--- src/components/common/ArticleForm.tsx
+++ src/components/common/ArticleForm.tsx
@@ -3,7 +3,7 @@
 // 외부 라이브러리
 import React, { useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Box, Input, Text, Flex, Button } from "@chakra-ui/react";
+import { Box, Input, Text, Flex, Button, Textarea } from "@chakra-ui/react";
 import EditorJS from "@editorjs/editorjs";
 import ImageTool from "@editorjs/image";
 import { uploadFileApi } from "@/src/api/RegisterArticle";
@@ -69,16 +69,14 @@ export default function ArticleForm({
     initializeEditor(initialContent);
 
     return () => {
-
       if (
         editorRef.current &&
         typeof editorRef.current.destroy === "function"
       ) {
         editorRef.current.destroy();
-
       }
       editorRef.current = null;
-  };
+    };
   }, []);
 
   const initializeEditor = (content: any[] = []) => {
@@ -153,6 +151,7 @@ export default function ArticleForm({
 
   const handleEditorSave = async () => {
     setisDisabled(true);
+    setTimeout(() => setisDisabled(false), 2000);
     if (editorRef.current) {
       try {
         const savedData = await editorRef.current.save();
@@ -173,7 +172,7 @@ export default function ArticleForm({
           return;
         }
         if (pathname.includes("/approvals") && isSignYes === false) {
-          window.alert("서명을 입력하세요")
+          window.alert("서명을 입력하세요");
           return;
         }
 
@@ -283,9 +282,18 @@ export default function ArticleForm({
         <Box flex={2}>
           <Text mb={2}>제목</Text>
           <Input
+            type="text"
             placeholder="제목을 입력하세요."
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            maxHeight={"200px"}
+            maxLength={80}
+            style={{
+              overflow: "hidden",
+              resize: "none", // 크기 조절 방지
+              minHeight: "40px", // 최소 높이
+              height: "40px", // 기본 높이
+            }}
           />
         </Box>
       </Flex>
