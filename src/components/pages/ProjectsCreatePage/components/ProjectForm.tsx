f82f24a32eabc91a161dc8701f2971c843e89094
Jeongjun <jeongjun.business@gmail.com>
2025-02-20 03:27:12 +0900

    프로젝트 유효성 검사 로직 별도 분리

--- src/components/pages/ProjectsCreatePage/components/ProjectForm.tsx
+++ src/components/pages/ProjectsCreatePage/components/ProjectForm.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/src/hook/useMutationData";
 import DateSection from "@/src/components/pages/ProjectsCreatePage/components/DateSection";
 import { showToast } from "@/src/utils/showToast";
+import { validateForm } from "@/src/hook/useValidation";
 
 interface ProjectFormProps {
   projectData?: ProjectDetailProps; // projectData가 있을 경우 수정 모드
@@ -142,88 +143,7 @@ export default function ProjectForm({
     formData.name = formData.name.trim().replace(/\s{2,}/g, " ");
     formData.description = formData.description.trim().replace(/\s{2,}/g, " ");
 
-    // 필수 정보
-    if (formData.name.length < 2) {
-      const errorMessage = "프로젝트명을 2글자 이상 입력해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (formData.description.length < 2) {
-      const errorMessage = "프로젝트 개요를 2글자 이상 입력해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.startAt) {
-      const errorMessage = "프로젝트 시작일을 선택해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.deadlineAt) {
-      const errorMessage = "프로젝트 예정 마감일을 선택해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.customerOrgId) {
-      const errorMessage = "고객사를 지정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.developerOrgId) {
-      const errorMessage = "개발사를 지정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (selectedCustomerMembers.length === 0) {
-      const errorMessage = "고객사 담당자 회원을 배정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (selectedDeveloperMembers.length === 0) {
-      const errorMessage = "개발사 담당자 회원을 배정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.customerOwnerId) {
-      const errorMessage = "고객사 Owner을 지정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    } else if (!formData.devOwnerId) {
-      const errorMessage = "개발사 Owner을 지정해주세요.";
-      showToast({
-        title: "필수 입력정보 ",
-        description: errorMessage,
-        duration: 2000,
-      });
-      return;
-    }
+    if (!validateForm(formData)) return; // 유효성 검사 실패 시 종료
 
     const requestBody = {
       ...formData,
@@ -264,7 +184,7 @@ export default function ProjectForm({
   return (
     <Flex width="100%" justifyContent="center">
       <InputFormLayout
-        title={isEditMode ? "프로젝트 상세 조회" : "프로젝트 생성"}
+        title={isEditMode ? "프로젝트 수정" : "프로젝트 생성"}
         onSubmit={(event) => handleSubmit(event)}
         isLoading={isSubmitting}
         isDisabled={false} // 버튼 비활성화 조건 추가
